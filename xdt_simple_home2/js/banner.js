c0429b0d2ffccc86a57c134f3bc8ffb45c30a959
Beom Jinhyeok <dennis4079@gmail.com>
2015-02-03 20:31:17 +0900

    Simple Home2 배너 작동 오류 수정

--- xdt_simple_home2/js/banner.js
+++ xdt_simple_home2/js/banner.js
@@ -1,3 +1,3 @@
-jQuery(document).ready(function(){
-			jQuery('#xdtbanner').xdtslider({'delay':5000, 'fadeSpeed': 3000,'showNextPrev':true,'showPlayButton':true,'autoStart':true});
+jQuery(document).ready(function($){
+			$('#xdtbanner').xdtslider({'delay':5000, 'fadeSpeed': 3000,'showNextPrev':true,'showPlayButton':true,'autoStart':true});
 		});
